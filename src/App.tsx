292c340c39742d21652bcd9307cb87b0675fcf94
Frenky Harry S. Sinaga <me@frenky.id>
2022-02-24 12:20:40 +0700

    fancy reformatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import abi from "./utils/PunchContractABI.json";
 
-
-import './App.css';
+import "./App.css";
 
 function App() {
   const contractABI = abi.abi;
   const contractAddress = "0x04b065b6B54Ef2171f36ceaE90E2689171514C17";
 
   type Punch = {
-    address: string,
-    timestamp: Date,
-    message: string
+    address: string;
+    timestamp: Date;
+    message: string;
   };
 
   const [currentAccount, setCurrentAccount] = useState<string>("");
@@ -27,7 +26,6 @@ function App() {
         console.log("Make sure you have metamask!");
       } else {
         console.log("We have the Ethereum object", ethereum);
-
 
         const accounts = await ethereum.request({ method: "eth_accounts" });
 
@@ -55,7 +53,9 @@ function App() {
         return;
       }
 
-      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
 
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
@@ -75,9 +75,9 @@ function App() {
         address: from,
         timestamp: new Date(timestamp * 1000),
         message: message,
-      }
-      setAllPunches(prevState => [...prevState, newPunch])
-    }
+      };
+      setAllPunches((prevState) => [...prevState, newPunch]);
+    };
 
     try {
       const { ethereum } = window as any;
@@ -89,7 +89,7 @@ function App() {
         contract.on("NewPunch", onNewPunch);
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
 
     return () => {
@@ -107,7 +107,11 @@ function App() {
       if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const contract = new ethers.Contract(contractAddress, contractABI, signer);
+        const contract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
 
         let count = await contract.getTotalPunches();
         console.log("Retreived total punch count...", count.toNumber());
@@ -135,7 +139,11 @@ function App() {
       if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const contract = new ethers.Contract(contractAddress, contractABI, signer);
+        const contract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
 
         let count = await contract.getTotalPunches();
         console.log("Retreived total punch count...", count.toNumber());
@@ -156,7 +164,11 @@ function App() {
       if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const contract = new ethers.Contract(contractAddress, contractABI, signer);
+        const contract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
 
         const allPunches = await contract.getAllPunches();
 
@@ -165,9 +177,9 @@ function App() {
           let curr: Punch = {
             address: x.puncher,
             timestamp: new Date(x.timestamp * 1000),
-            message: x.message
-          }
-          punchesCleaned.push(curr)
+            message: x.message,
+          };
+          punchesCleaned.push(curr);
         });
 
         setAllPunches(punchesCleaned);
@@ -186,11 +198,11 @@ function App() {
   return (
     <div className="flex justify-center mt-10">
       <div className="flex flex-col justify-center max-w-lg">
-        <div className="text-center text-2xl font-semibold mt-4">
+        <div className="mt-4 text-2xl font-semibold text-center">
           👋 Hey there! 👊 Punch Me!
         </div>
 
-        <div className="text-center text-gray-700 mt-10">
+        <div className="mt-10 text-center text-gray-700">
           I deployed a bug to production, I should be punched!
           <p>I got punched for {punchCount} times so far!</p>
           <p className="mt-2 text-sm font-semibold">
@@ -199,32 +211,53 @@ function App() {
         </div>
 
         {!currentAccount && (
-          <button className="cursor-pointer mt-4 p-2 border-1 bg-blue-600 rounded-md text-white hover:bg-blue-700" onClick={connectWallet}>
+          <button
+            className="p-2 mt-4 text-white bg-blue-600 rounded-md cursor-pointer border-1 hover:bg-blue-700"
+            onClick={connectWallet}
+          >
             Connect MetaMask
           </button>
         )}
 
         <div className="flex flex-col mt-10 text-gray-700">
-          <label className="text-gray-700 p-1">Send message</label>
-          <input className="p-3 border rounded-md border-gray-400" onChange={e => updateMessage(e)} placeholder="Send me a message with your punch..."></input>
+          <label className="p-1 text-gray-700">Send message</label>
+          <input
+            className="p-3 border border-gray-400 rounded-md"
+            onChange={(e) => updateMessage(e)}
+            placeholder="Send me a message with your punch..."
+          ></input>
         </div>
 
-        <button className="cursor-pointer mt-4 px-2 py-4 border-1 bg-orange-600 rounded-lg font-bold text-white hover:bg-orange-700" onClick={doPunch}>
+        <button
+          className="px-2 py-4 mt-4 font-bold text-white bg-orange-600 rounded-lg cursor-pointer border-1 hover:bg-orange-700"
+          onClick={doPunch}
+        >
           1x Punch! 👊
         </button>
 
         <div className="mt-5">
           {allPunches.map((punch, index) => {
             return (
-              <div className="py-3 px-3 bg-orange-300 mb-3 rounded-md" key={index}>
-                <div className="bg-orange-700/40 rounder rounded-md px-3 py-2 italic text-sm text-gray-100">
+              <div
+                className="px-3 py-3 mb-3 bg-orange-300 rounded-md"
+                key={index}
+              >
+                <div className="px-3 py-2 text-sm italic text-gray-100 rounded-md bg-orange-700/40 rounder">
                   {punch.message}
                 </div>
-                <div className="flex flex-col mx-1 mt-2 text-sm pl-1">
-                  <a className="hover:text-gray-700" href={"https://rinkeby.etherscan.io/address/" + punch.address}>{punch.address}</a>
+                <div className="flex flex-col pl-1 mx-1 mt-2 text-sm">
+                  <a
+                    className="hover:text-gray-700"
+                    href={
+                      "https://rinkeby.etherscan.io/address/" + punch.address
+                    }
+                  >
+                    {punch.address}
+                  </a>
                   <div className="">{punch.timestamp.toLocaleString()}</div>
                 </div>
-              </div>)
+              </div>
+            );
           })}
         </div>
       </div>
